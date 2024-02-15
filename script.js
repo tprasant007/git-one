c5597d9433f69d577e0ae434c3ca0d01deace0b3
Prashant Thapa <tprasant10@gmail.com>
2024-02-15 01:38:41 +0000

    added feature 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById("primary-btn").addEventListener("click", ()=> {
     document.getElementById("example").innerHTML = "Why did u click you idiot???"
 })
+
+console.log("feature 1")
